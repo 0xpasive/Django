42d408d7c9b000fad5ebba19b76f23606fdba7d3
0xpasive <118454575+0xpasive@users.noreply.github.com>
2024-04-22 12:30:03 +0000

    change

---
+++ epasal/store/views.py
@@ -0,0 +1,372 @@
+from typing import Any
+from django.forms.models import BaseModelForm
+from django.shortcuts import render
+from django.views.generic import TemplateView , View , CreateView , FormView
+from .models import *
+from decimal import Decimal
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import redirect
+from .forms import CustomerRegistrationForm, CustomerLoginForm
+from django.contrib.auth import authenticate, login, logout 
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+
+
+
+class storeMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        cart_id = request.session.get("cart_id", None)
+        if cart_id:
+            cart_obj = Cart.objects.get(id=cart_id)
+            if request.user.is_authenticated and request.user.customer:
+                cart_obj.customer = request.user.customer
+                cart_obj.save()
+        return super().dispatch(request, *args, **kwargs)
+
+class HomeView(storeMixin,TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) #kwargs is a dictionary of all the arguments passed to the view it is used to pass additional data to the template
+        all_products = Product.objects.order_by("-id")
+        paginator = Paginator(all_products, 7) 
+        page_number = self.request.GET.get("page")
+        product_list = paginator.get_page(page_number)
+        context['product_list'] = product_list
+        return context
+
+class GroceryView(storeMixin,TemplateView):
+    template_name = "groceries.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Category.objects.get(title="Groceries").product_set.all()
+        return context
+    
+class SnacksView(storeMixin,TemplateView):
+    template_name = "snacks.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Category.objects.get(title="Snacks").product_set.all()
+        return context
+    
+class BeveragesView(storeMixin,TemplateView):
+    template_name = "beverages.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Category.objects.get(title="Beverages").product_set.all()
+        return context
+
+class HomecareView(storeMixin,TemplateView):
+    template_name = "personalandhomecare.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Category.objects.get(title="Personal care and hygiene").product_set.all()
+        return context
+
+class PackagedfoodsView(storeMixin,TemplateView):
+    template_name = "packagedfoods.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Category.objects.get(title="packaged food").product_set.all()
+        return context
+
+class StationariesView(storeMixin,TemplateView):
+    template_name = "stationaries.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Category.objects.get(title="stationary").product_set.all()
+        return context
+
+class OthersView(storeMixin,TemplateView):
+    template_name = "others.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Category.objects.get(title="others").product_set.all()
+        return context
+    
+class ProductDetailView(storeMixin,TemplateView):
+    template_name = "productdetail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = kwargs['slug']
+        context['product'] = Product.objects.get(slug=slug)
+        
+        return context
+
+class AddToCartView(storeMixin,TemplateView):  
+    
+
+    def get(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #get product id from url
+        pro_id = kwargs['pro_id']
+
+        #get product
+        product_obj = Product.objects.get(id=pro_id)
+        #check if cart exists
+        cart_id = self.request.session.get("cart_id",None)
+        if cart_id:
+            cart_obj =Cart.objects.get(id=cart_id)
+            this_product_in_cart = cart_obj.cartproduct_set.filter(product=product_obj)
+            if this_product_in_cart.exists():
+                cartproduct = this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal += product_obj.price
+                cartproduct.save()
+                cart_obj.total += product_obj.price
+                cart_obj.save()
+            else:
+                cartproduct = CartProduct.objects.create(
+                    cart=cart_obj,
+                    product=product_obj,
+                    rate=product_obj.price,
+                    quantity=1,
+                    subtotal=product_obj.price
+                )
+                cart_obj.total = Decimal(cart_obj.total)
+                cart_obj.total += product_obj.price
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.create(cart_id=1)
+            self.request.session['cart_id'] = cart_obj.id
+            cartproduct = CartProduct.objects.create(
+                cart=cart_obj,
+                product=product_obj,
+                rate=product_obj.price,
+                quantity=1,
+                subtotal=product_obj.price
+            )
+            cart_obj.total = Decimal(cart_obj.total)
+            cart_obj.total += product_obj.price
+            cart_obj.save()
+
+        return redirect(reverse("store:mycart"))
+    
+class MyCartView(storeMixin,TemplateView):
+    template_name = "mycart.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id",None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+        else:
+            cart = None
+        context['cart'] = cart
+        return context
+    
+class ManageCartView(storeMixin,View):
+    def get(self, request, *args, **kwargs):
+        cp_id = self.kwargs["cp_id"]
+        action = request.GET.get("action")
+        cp_obj = CartProduct.objects.get(id=cp_id)
+        cart_obj = cp_obj.cart
+        if action == "inc":
+            cp_obj.quantity += 1
+            cp_obj.subtotal += cp_obj.rate
+            cp_obj.save()
+            cart_obj.total += cp_obj.rate
+            cart_obj.save()
+        elif action == "dcr":
+            cp_obj.quantity -= 1
+            cp_obj.subtotal -= cp_obj.rate
+            cp_obj.save()
+            cart_obj.total -= cp_obj.rate
+            cart_obj.save()
+            if cp_obj.quantity == 0:
+                cp_obj.delete()
+        elif action == "rmv":
+            cart_obj.total -= cp_obj.subtotal
+            cart_obj.save()
+            cp_obj.delete()
+        else:
+            pass
+        return redirect(reverse("store:mycart"))
+    
+class EmptyCartView(storeMixin,View):
+    def get(self, request, *args, **kwargs):
+        cart_id = self.request.session.get("cart_id",None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+            cart.cartproduct_set.all().delete()
+            cart.total = 0
+            cart.save()
+        return redirect(reverse("store:mycart"))
+    
+class CheckoutView(storeMixin,TemplateView):
+    template_name = "checkout.html"
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated and request.user.customer:
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            #after login user will be redirected to checkout page
+            return redirect(reverse("store:customerlogin") + "?next=/checkout/")
+
+            
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id",None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+        else:
+            cart = None
+        context['cart'] = cart
+        return context
+
+    # get detail from the form and save it to the database
+    def post(self, request, *args, **kwargs):
+        cart_id = self.request.session.get("cart_id",None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+        else:
+            cart = None
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        address = request.POST.get("address")
+        
+        order = Order.objects.create(
+            cart=cart,
+            name=name,
+            email=email,
+            phone=phone,
+            address=address,
+            order_id=cart.cart_id
+            
+        )
+        del request.session['cart_id']
+        return redirect(reverse("store:home"))
+    
+
+
+
+    
+
+class CustomerRegistrationView(CreateView):
+    template_name = "customerregistration.html"
+    form_class = CustomerRegistrationForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        
+        email = form.cleaned_data.get("email")
+
+        user = User.objects.create_user(username, email, password)
+        form.instance.user = user
+        login(self.request, user)
+
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        next_url = self.request.GET.get("next")
+        if next_url:
+            return next_url
+        else:
+            return self.success_url
+    
+class CustomerLogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect(reverse("store:home"))
+    
+class CustomerLoginView(FormView):
+    template_name = "customerlogin.html"
+    form_class = CustomerLoginForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        uname = form.cleaned_data.get("username")
+        pword = form.cleaned_data.get("password")
+        usr = authenticate(username=uname, password=pword)
+        if usr is not None and usr.customer:
+            login(self.request, usr)
+        else:
+            return render(self.request, self.template_name, {"form":form, "error":"Invalid Credentials"})
+
+        
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        next_url = self.request.GET.get("next")
+        if next_url:
+            return next_url
+        else:
+            return self.success_url
+
+
+
+class CustomerProfileView(TemplateView):
+    template_name = "customerprofile.html"
+    def dispatch(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated and request.user.customer:
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            #after login user will be redirected to checkout page
+            return redirect(reverse("store:customerlogin") + "?next=/profile/")
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        customer = self.request.user.customer
+        context['customer'] = customer
+        orders = Order.objects.filter(cart__customer=customer).order_by("-id")
+        context['orders'] = orders
+
+        return context
+
+   
+class CancelOrderView(View):
+    def get(self, request, *args, **kwargs):
+        order_id = self.kwargs['cp_id']
+        order = Order.objects.get(id=order_id)
+        order.status = "Order Cancelled"
+        order.save()
+        return redirect(reverse("store:customerprofile"))
+    
+
+class CustomerOrderDetailView(TemplateView):
+    template_name = "orderdetail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order_id = self.kwargs['cp_id']
+        order = Order.objects.get(id=order_id)
+        context['order'] = order
+        return context
+        
+
+class SearchView(TemplateView):
+    template_name = "search.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        kw = self.request.GET.get("keyword")
+        results = Product.objects.filter(Q(name__contains=kw) | Q(description__contains=kw))
+        context['results'] = results
+        return context
+
+
+
+
+        
+    
+    
+
+    
+    
